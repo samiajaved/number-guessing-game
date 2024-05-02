de7df412947762116c9d630988c3e55a67583834
samiajaved <samiaj380@gmail.com>
2024-05-02 00:17:56 -0700

    push my project number-guessing-game

---
+++ main.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answer = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guess a number between 1 to 10: "
+    }
+]);
+if (answer.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you gussed the right number");
+}
+else {
+    console.log("you gussed wrong number.");
+}
+console.log(`I guessed ${randomNumber}`);
